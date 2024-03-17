c61a1ab9411c46ec239b5df4ecdd22e35381afa4
Venkatesh Pothuraju <140961144+Venky2204@users.noreply.github.com>
2024-03-17 19:57:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 const express =require('express');
 const app = express();
 const mongoose = require("mongoose");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./keys');
 // const cors = require('cors');
 
 // app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
